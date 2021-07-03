545ee7a1cfa591a265b0e6a6eee01f1a0ad936c7
Nasif Shah <33353457+nasifshah2017@users.noreply.github.com>
2021-07-03 11:44:55 -0300

    Update frozenReducer.js

--- src/reducers/frozenReducer.js
+++ src/reducers/frozenReducer.js
@@ -42,7 +42,7 @@ export default (state = seedData, action) => {
 
     } else if (action.type === 'clearInventory') {
         let newState = [...state];
-        newState.forEact((item, i) => {
+        newState.forEach((item, i) => {
             item.quantity = 0;
         })
         return newState;
